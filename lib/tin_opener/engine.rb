7847b9dd2db5655436144b1c126d6078fe48c030
David Gil <dgilperez@gmail.com>
2015-10-24 21:39:29 +0200

    generators configuration

--- lib/tin_opener/engine.rb
+++ lib/tin_opener/engine.rb
@@ -1,5 +1,17 @@
 module TinOpener
   class Engine < ::Rails::Engine
     isolate_namespace TinOpener
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+
+      g.view_specs       false
+      g.helper_specs     false
+      g.controller_specs false
+    end
   end
 end
