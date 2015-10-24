5883ed707743f50fbf95103e81c1430963250739
David Gil <dgilperez@gmail.com>
2015-10-24 22:35:13 +0200

    adds some validations

--- app/models/tin_opener/data_set.rb
+++ app/models/tin_opener/data_set.rb
@@ -1,5 +1,8 @@
 module TinOpener
   class DataSet < ActiveRecord::Base
     has_many :records, dependent: :destroy
+
+    validates :headers, presence: true
+    validates :name, presence: true
   end
 end
--- app/models/tin_opener/record.rb
+++ app/models/tin_opener/record.rb
@@ -1,5 +1,7 @@
 module TinOpener
   class Record < ActiveRecord::Base
     belongs_to :data_set
+
+    validates :row_data, presence: true
   end
 end
